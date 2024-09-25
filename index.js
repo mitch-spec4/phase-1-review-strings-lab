b7e1389510b2da74d721046071eae24f1972831e
mitch kamau <michchore@gmail.com>
2024-09-25 20:22:49 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code in this file!
+// Write your code in this 
+const currentUser ="Mitch"
+
+const welcomeMessage =`Welcome to Flatbook, ${currentUser}!`
+//console.log(`${currentUser} ${welcomeMessage}`)
+//console.log(currentUser + " " + welcomeMessage)
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const firstInitial = currentUser [0];
+const shortGreeting= `Welcome, ${firstInitial}!`;
+ //const shortGreetings= `Welcome,${firstInitial}!`
